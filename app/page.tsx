c48c91277fb270c9b978c3ca3717fdb488f0e886
Anas <muhammadanasbinadam@gmail.com>
2025-03-18 22:35:47 +0800

    Integrate with supabase

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,62 @@
+"use client"
+
 import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Card, CardContent } from "@/components/ui/card";
 import Image from "next/image";
 import SellixLogo from "../public/sellix.svg";
+import { useState } from "react";
+
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+import supabase from "@/utils/supabase";
+import { CircleCheck, Loader2 } from "lucide-react";
+ 
+const formSchema = z.object({
+  email: z.string().email("Please enter a valid email address"),
+})
 
 export default function Home() {
+  const [isLoading, setIsLoading] = useState(false);
+  const [isComplete, setIsComplete] = useState(false);
+
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+    },
+  })
+ 
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    setIsLoading(true);
+    joinWaitlist(values.email);
+    form.reset();
+  }
+
+  const joinWaitlist = async (email: string) => {
+    const { error } = await supabase
+      .from('waitlist')
+      .insert({ email: email })
+
+    if (!error) {
+      console.log('insert success');
+      setIsLoading(false);
+      setIsComplete(true);
+    } else {
+      console.log('error');
+      setIsLoading(false);
+    }
+  }
+
   return (
     <div className="flex flex-col items-center justify-center min-h-screen bg-gray-50 p-6">
 
@@ -16,9 +68,40 @@ export default function Home() {
       <h1 className="text-4xl lg:text-6xl text-slate-700 font-bold mb-4 max-w-2xl text-center">Turn Your Bio Link into a Shoppable Store</h1>
       <p className="text-base text-gray-600 mt-2 mb-6">Sell products, track analytics, and accept payments directly from your bio link.</p>
       
-      <div className="flex space-x-4">
-        <Input placeholder="Enter your email" className="w-64" />
-        <Button>Join the Waitlist</Button>
+      <div className="w-full sm:w-fit">
+        {!isComplete ?
+          <Form {...form}>
+            <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+              <FormField
+                control={form.control}
+                name="email"
+                render={({ field }) => (
+                  <FormItem>
+                    <div className="flex flex-col lg:flex-row space-4 gap-3 w-full md:w-fit">
+                      <FormControl>
+                        <Input placeholder="Enter your email" {...field} className="w-full sm:w-64" />
+                      </FormControl>
+                      <Button type="submit" disabled={isLoading}>
+                        {isLoading &&
+                          <Loader2 className="animate-spin" />
+                        }
+                        Join the Waitlist
+                      </Button>
+                    </div>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+            </form>
+          </Form>
+          :
+          <div className="flex flex-col gap-2">
+            <CircleCheck className="w-10 h-10 text-green-500 mx-auto" />
+            <p className="text-center">
+              Yay, you're on the waitlist!
+            </p>
+          </div>
+        }
       </div>
       
       <div className="grid grid-cols-1 md:grid-cols-3 gap-6 mt-12">
