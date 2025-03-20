deab33ff5e479aad77355ad989b940f7fd6ac622
Anas <muhammadanasbinadam@gmail.com>
2025-03-20 22:12:13 +0800

    Update meta tag

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,18 +1,22 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
-import MetaImage from "../public/meta-image.svg";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Stellium - Shoppable Link in Bio for E-Commerce",
   description: "Turn your bio into a sales powerhouse with Stellium. Create shoppable links, track real-time analytics, and accept payments via Stripe & PayPal – all in one simple tool.",
-  twitter: {
-    card: 'summary_large_image',
+  keywords: "link in bio, shop, online shop, online store",
+  openGraph: {
     title: 'Stellium - Shoppable Link in Bio for E-Commerce',
     description: 'Turn your bio into a sales powerhouse with Stellium. Create shoppable links, track real-time analytics, and accept payments via Stripe & PayPal – all in one simple tool.',
-    images: [MetaImage],
+    images: "/meta-image.png"
+  },
+  twitter: {
+    title: 'Stellium - Shoppable Link in Bio for E-Commerce',
+    description: 'Turn your bio into a sales powerhouse with Stellium. Create shoppable links, track real-time analytics, and accept payments via Stripe & PayPal – all in one simple tool.',
+    images: "/meta-image.png"
   },
 };
 
