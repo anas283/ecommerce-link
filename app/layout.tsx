23df81929678e583889d93a282107f3577d9a994
Anas <muhammadanasbinadam@gmail.com>
2025-03-20 22:06:16 +0800

    Update meta card

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,19 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import MetaImage from "../public/meta-image.svg";
 
 const inter = Inter({ subsets: ['latin'] })
 
 export const metadata: Metadata = {
   title: "Stellium - Shoppable Link in Bio for E-Commerce",
   description: "Turn your bio into a sales powerhouse with Stellium. Create shoppable links, track real-time analytics, and accept payments via Stripe & PayPal – all in one simple tool.",
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Stellium - Shoppable Link in Bio for E-Commerce',
+    description: 'Turn your bio into a sales powerhouse with Stellium. Create shoppable links, track real-time analytics, and accept payments via Stripe & PayPal – all in one simple tool.',
+    images: [MetaImage],
+  },
 };
 
 export default function RootLayout({
